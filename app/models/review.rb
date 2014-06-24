20981e58738f895b46d473a00dfd46aaa5db1730
Jacinda Zhong <jacindaz@gmail.com>
2014-06-24 14:24:17 -0400

    Review model with validations and meme association

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,13 +2,22 @@ class Review < ActiveRecord::Base
 
   validates_presence_of :user_id, :image_id, :rating, :title, :body
 
-  validates :user_id
-  validates :image_id
+  validates :user_id, numericality: { integer: true }
+  validates :image_id, numericality: { integer: true }
   validates :rating, numericality: { integer: true, greater_than_or_equal_to: 1,
                                       less_than_or_equal_to: 5 }
-  validates :title
-  validates :body
+  validates :title, numericality: { integer: false },
+            length: { minimum: 10, maximum: 90 }
+  validates :body, length: {
+    minimum: 5,
+    maximum: 300,
+    tokenizer: lambda { |str| str.scan(/\w+/) },
+    too_short: "Must have at least %{count} words.",
+    too_long: "Must have less than %{count} words."
+  }
 
-  validates_inclusion_of :rating, :in => 1..5
+  # belongs_to :user
+  # has_many :votes
+  belongs_to :meme
 
 end
