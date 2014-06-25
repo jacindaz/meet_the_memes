bd2a071375e93c0ccc6112117fb46f382ce3d205
Dan Murphy <murphydbuffalo@gmail.com>
2014-06-25 14:06:31 -0400

    Passed tests for editing a user account.

--- spec/features/devise/user_changes_account_info_spec.rb
+++ spec/features/devise/user_changes_account_info_spec.rb
@@ -31,4 +31,94 @@ feature 'User edits account info.' do
     expect(updated_user.username).to eq('coffeeplants5')
   end
 
+  scenario 'User enters an invalid password.' do
+    user = FactoryGirl.create(:user)
+
+    visit new_user_registration_path
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    fill_in 'Password confirmation', with: user.password
+    fill_in 'Username', with: user.username
+    click_on 'Done'
+
+    visit new_user_session_path
+
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_on 'Done' 
+
+    visit edit_user_registration_path
+
+    fill_in 'Email', with: 'craig@greig.com'
+    fill_in 'Password', with: '123'
+    fill_in 'Password confirmation', with: '123'
+    fill_in 'Current password', with: user.password
+    fill_in 'Username', with: 'coffeeplants5'
+    click_on 'Update' 
+
+    expect(page).to have_content('too short')
+    updated_user = User.first
+    expect(updated_user.email).to_not eq('craig@greig.com')
+  end
+
+  scenario 'Passwords don\'t match.' do
+    user = FactoryGirl.create(:user)
+
+    visit new_user_registration_path
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    fill_in 'Password confirmation', with: user.password
+    fill_in 'Username', with: user.username
+    click_on 'Done'
+
+    visit new_user_session_path
+
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_on 'Done' 
+
+    visit edit_user_registration_path
+
+    fill_in 'Email', with: 'craig@greig.com'
+    fill_in 'Password', with: '12345678910'
+    fill_in 'Password confirmation', with: '10987654321'
+    fill_in 'Current password', with: user.password
+    fill_in 'Username', with: 'coffeeplants5'
+    click_on 'Update' 
+
+    expect(page).to have_content('doesn\'t match')
+    updated_user = User.first
+    expect(updated_user.email).to_not eq('craig@greig.com')
+  end
+
+  scenario 'User enters an invalid username.' do
+    user = FactoryGirl.create(:user)
+
+    visit new_user_registration_path
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    fill_in 'Password confirmation', with: user.password
+    fill_in 'Username', with: user.username
+    click_on 'Done'
+
+    visit new_user_session_path
+
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_on 'Done' 
+
+    visit edit_user_registration_path
+
+    fill_in 'Email', with: 'craig@greig.com'
+    fill_in 'Password', with: '12345678910'
+    fill_in 'Password confirmation', with: '12345678910'
+    fill_in 'Current password', with: user.password
+    fill_in 'Username', with: 'c'
+    click_on 'Update' 
+
+    expect(page).to have_content('too short')
+    updated_user = User.first
+    expect(updated_user.email).to_not eq('craig@greig.com')
+  end
+
 end
