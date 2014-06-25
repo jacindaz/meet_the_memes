d6d2a552c6c2bf403597bc03f109195d1c28ece6
Adam Andrewjeski <aandrewjeski@gmail.com>
2014-06-25 17:06:56 -0400

    Addedcarrierwave image upload functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  mount :profile_picture, ProfilePictureUploader
+
   validates :email, :password, :password_confirmation, :username, presence: true
   validates :password, confirmation: true, length: {minimum: 10}
   validates :username, length: {minimum: 2, maximum: 25}
--- db/schema.rb
+++ db/schema.rb
@@ -11,44 +11,47 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140624204819) do
+ActiveRecord::Schema.define(version: 20140625204849) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "memes", force: true do |t|
-    t.string   "url", null: false
-    t.string   "name", null: false
+    t.string   "url",            null: false
+    t.string   "name",           null: false
     t.text     "description"
     t.float    "average_rating"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
+  create_table "reviews", force: true do |t|
+    t.integer  "meme_id",    null: false
+    t.integer  "rating",     null: false
+    t.string   "title",      null: false
+    t.text     "body",       null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "users", force: true do |t|
-    t.string   "email", default: "", null: false
-    t.string   "encrypted_password", default: "", null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count", default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.string   "username", null: false
+    t.string   "username",                            null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "profile_picture"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-
-  create_table "reviews", force: true do |t|
-    t.integer  "meme_id", null: false
-    t.integer  "rating", null: false
-    t.string   "title", null: false
-    t.text     "body", null: false
-
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
 
 end
