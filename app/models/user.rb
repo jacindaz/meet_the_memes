58140240b91e914b223785d58b1c530e2dcb39bb
craigmcginley <craigmcginley@gmail.com>
2014-06-26 15:00:24 -0400

    made user cascade delete reviews. memes can have anon user if user is deleted

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
   has_many :memes
 
   validates :email, :password, :password_confirmation, :username, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140626173042) do
+ActiveRecord::Schema.define(version: 20140626185126) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,7 +23,7 @@ ActiveRecord::Schema.define(version: 20140626173042) do
     t.float    "average_rating"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "user_id",        null: false
+    t.integer  "user_id"
   end
 
   create_table "reviews", force: true do |t|
