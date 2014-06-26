a9c45355654e4c6fa8d478102cda59fbf2fa6681
Jacinda Zhong <jacindaz@gmail.com>
2014-06-26 14:31:16 -0400

    Adds vote model with scope and inclusion validations

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,8 +1,10 @@
 class Vote < ActiveRecord::Base
 
-  validates :user_id, presence: true
-  validates :review_id, presence: true
+  validates :user_id, presence: true, numericality: { integer: true }
+  validates :review_id, presence: true, numericality: { integer: true }
   validates :type, presence: true
+  validates_inclusion_of :type, :in => [-1, 1]
+  validates_uniqueness_of :user_id, scope: :review_id
 
   belongs_to :user
   belongs_to :review
