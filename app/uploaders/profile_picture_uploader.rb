99d981ba8cc6adbde590a22acf5d2ad36e79bd1a
Dan Murphy <murphydbuffalo@gmail.com>
2014-06-26 14:26:21 -0400

    Added default images for useres and memes.

--- app/uploaders/profile_picture_uploader.rb
+++ app/uploaders/profile_picture_uploader.rb
@@ -4,7 +4,8 @@ class ProfilePictureUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
+  process resize_to_fit: [450,450]
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -13,16 +14,16 @@ class ProfilePictureUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "public/uploads/#{model.class.to_s}/#{mounted_as}/#{model.id}"
+    "uploads/#{model.class.to_s}/#{mounted_as}/#{model.id}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url
-  #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  #
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
+  def default_url
+    # For Rails 3.1+ asset pipeline compatibility:
+    ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.jpg"].compact.join('_'))
+  
+    #"/images/fallback/" + [version_name, "default.jpg"].compact.join('_')
+  end
 
   # Process files as they are uploaded:
   # process :scale => [200, 300]
