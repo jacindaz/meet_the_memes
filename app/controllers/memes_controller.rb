927df167c0bb40427866103f6ae1d37d59293d01
craigmcginley <craigmcginley@gmail.com>
2014-07-01 15:20:06 -0400

    made some fixes

--- app/controllers/memes_controller.rb
+++ app/controllers/memes_controller.rb
@@ -1,5 +1,5 @@
 class MemesController < ApplicationController
-
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   def index
     order_possibilities = ["name", "average_rating"]
 
