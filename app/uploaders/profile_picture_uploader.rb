e6f91ad024e34d024863f5ca14eaf854a4973ec3
Dan Murphy <murphydbuffalo@gmail.com>
2014-07-01 11:05:40 -0400

    Allowed image processing in the test and dev environments (resizes images to smaller sizes) to help with styling and page layout.

--- app/uploaders/profile_picture_uploader.rb
+++ app/uploaders/profile_picture_uploader.rb
@@ -8,7 +8,7 @@ class ProfilePictureUploader < CarrierWave::Uploader::Base
   process resize_to_fit: [450,450]
 
   version :thumbnail do
-    process resize_to_fit: [100,100]
+    process resize_to_fit: [175,175]
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
