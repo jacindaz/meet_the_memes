d4f826b70b98e150d837247231560d81fdb21830
Dan Murphy <murphydbuffalo@gmail.com>
2014-07-02 11:59:45 -0400

    Seeded db with a series of pictures and reviews.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,26 @@
+pictures = Dir.entries("#{Rails.root}/db/seeds")
+
+pictures.each_with_index do |pic, i|
+  if pic[-3..-1] == 'jpg' || pic[-3..-1] == 'png'
+    meme = Meme.create(
+      name: "This is a name for meme ##{i - 1}!",
+      url: 'http://www.google.com',
+      description: 'So wow. Such meme. Many English. Very doge. Much tube-shaped animal.',
+      picture: File.open(File.join("#{Rails.root}/db/seeds/#{pic}")),
+      user_id: 11
+      )
+    Review.create(
+      rating: rand(1..5),
+      title: "This isn't not the title! So wow. #{i}",
+      body: "OMG teh meme!  First! Other internet idiosyncrasies! Much doge. Such doge.",
+      user_id: 1,
+      meme_id: meme.id
+      )
+  end
+end
+# Rails' form_for helper automatically multi-part encodes uploads. Rake commands like db:seed do not.
+# Must use File.open('this/is/the/filepath') to get around this.
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
